b73c67a4bb0952f239a110f4668a1ee161c1833e
Jafar Shaikh <jafar@datavoice.co.in>
2024-09-01 20:28:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import XTable from './XTable';
+
+function App() {
+  return (
+    <div className="App">
+     <XTable />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/XTable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const XTable = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default XTable
